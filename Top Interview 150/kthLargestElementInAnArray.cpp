3a6f10b09869439b42dfa38446f80612b56cdfde
Nafis Tanbin <tanbinnafis@gmail.com>
2024-09-30 11:19:08 +0600

    Added new file

---
+++ Top Interview 150/kthLargestElementInAnArray.cpp
@@ -0,0 +1,21 @@
+/*
+https://leetcode.com/problems/kth-largest-element-in-an-array/
+*/
+
+class Solution
+{
+public:
+    int findKthLargest(vector<int> &nums, int k)
+    {
+        priority_queue<int> pq;
+        for (int i : nums)
+        {
+            pq.push(i);
+        }
+        for (int i = 0; i < k - 1; i++)
+        {
+            pq.pop();
+        }
+        return pq.top();
+    }
+};
