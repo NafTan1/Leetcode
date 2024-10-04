2ac0408f5277681a422d50139b8a0279a9fc49e2
Nafis Tanbin <tanbinnafis@gmail.com>
2024-10-04 16:51:50 +0600

    Added new files

---
+++ Top Interview 150/validParantheses.cpp
@@ -0,0 +1,41 @@
+/*
+Problem Link: https://leetcode.com/problems/valid-parentheses/
+*/
+
+class Solution
+{
+public:
+    bool isValid(string s)
+    {
+        stack<char> st;
+        for (char c : s)
+        {
+            if (c == '{' || c == '[' || c == '(')
+            {
+                st.push(c);
+            }
+            else
+            {
+                if (st.empty())
+                {
+                    return false;
+                }
+                else if (st.top() == '(' && c == ')')
+                {
+                    st.pop();
+                }
+                else if (st.top() == '{' && c == '}')
+                {
+                    st.pop();
+                }
+                else if (st.top() == '[' && c == ']')
+                {
+                    st.pop();
+                }
+                else
+                    return false;
+            }
+        }
+        return st.empty();
+    }
+};
